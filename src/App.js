24d4132e876be6cd6c2702a8f8603ebaac0daca5
dkagh012 <qowjdxo00@gmail.com>
2023-06-25 00:27:59 +0900

    react Card 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ExpensItem from './components/ExpenseItem';
-
+import Expenses from './components/Expenses';
+import './App.css';
 export default function App() {
   const expenses = [
     { id: 'e1', title: 'test1', amount: 123, date: new Date(2021, 2, 28) },
@@ -12,27 +12,7 @@ export default function App() {
   return (
     <div>
       <p>React 연습 중...</p>
-      <h1>연습 완료!</h1>
-      <ExpensItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpensItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpensItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpensItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses item={expenses} />
     </div>
   );
 }
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './ExpenseDate.css';
 function ExpenseDate(props) {
-  console.log(props);
-
   const month = props.date?.toLocaleString('en-US', { month: 'long' });
   const day = props.date?.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date?.getFullYear();
