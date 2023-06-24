254d218c2c5d2eb8bf742faa77d56784f0c2f02f
dkagh012 <qowjdxo00@gmail.com>
2023-06-24 20:38:12 +0900

    date를 js 방식으로

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './ExpnseItem.css'
 
 function ExpensItem(props) {
+  const month = props.date.toLocaleString('en-Us', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
