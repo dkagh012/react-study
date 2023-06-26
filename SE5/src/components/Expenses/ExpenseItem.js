b0a46513d809dc8559e60c81761e2cba9459eac0
dkagh012 <qowjdxo00@gmail.com>
2023-06-26 19:19:56 +0900

    useState 사용

--- SE5/src/components/Expenses/ExpenseItem.js
+++ SE5/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Update!');
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change Button!</button>
       </div>
     </Card>
   );
