c284b1b9595117059cb722f6bce737014a6e3652
dkagh012 <qowjdxo00@gmail.com>
2023-07-01 22:18:41 +0900

    차트 추가

---
+++ SE5/src/components/Chart/Chart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css"
+function Chart(props) {
+  return (
+    <div className="chart">
+      {props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={null} label={dataPoint.label} />)}
+    </div>
+  )
+}
+export default Chart;
