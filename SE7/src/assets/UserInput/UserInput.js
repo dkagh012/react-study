dd4e024e18b4beac4e015b624f67d34a81d4e184
Bae Jung Tae <qowjdxo00@gmail.com>
2023-07-03 19:11:17 +0900

    이벤트 처리

--- SE7/src/assets/UserInput/UserInput.js
+++ SE7/src/assets/UserInput/UserInput.js
@@ -1,14 +1,35 @@
 function UserInput() {
+  const submitHandler = (evnet) => {
+    evnet.preventDefault();
+  };
+  const resetHandler = () => {};
+  const inputChangeHandler = (input, value) => {};
   return (
-    <form className="form">
+    <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={
+              (event) =>
+                inputChangeHandler("current-savings", event.target.value)
+              // 첫번쨰는 input 값 두번째는 value값
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={
+              (event) =>
+                inputChangeHandler("yearly-contribution", event.target.value)
+              // 첫번쨰는 input 값 두번째는 value값
+            }
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -16,15 +37,30 @@ function UserInput() {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            onChange={
+              (event) =>
+                inputChangeHandler("expected-return", event.target.value)
+              // 첫번쨰는 input 값 두번째는 value값
+            }
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            onChange={
+              (event) => inputChangeHandler("duration", event.target.value)
+              // 첫번쨰는 input 값 두번째는 value값
+            }
+            type="number"
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button onClick={resetHandler} type="reset" className="buttonAlt">
           Reset
         </button>
         <button type="submit" className="button">
